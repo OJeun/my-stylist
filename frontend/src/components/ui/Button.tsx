f38f2344eb2fd4726d66fc6c240b94c1b27d5ce7
Diane Choi <ekska21@gmail.com>
2024-06-28 16:52:38 -0700

    Create custom button component

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,14 +1,58 @@
-type ButtonProps = {
-  el: 'button';
-}
+import { type ComponentPropsWithoutRef } from 'react';
 
-type AnchorProps = {
-  el: 'anchor';
+type ColorProps = {
+  color: 'primary' | 'secondary' | 'onlyText';
+  additionalClassName?: string;
+};
+
+type ButtonProps = ComponentPropsWithoutRef<'button'> &
+  ColorProps & {
+    href?: never;
+  };
+
+type AnchorProps = ComponentPropsWithoutRef<'a'> &
+  ColorProps & {
+    href?: string;
+  };
+
+// type predicate
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
+  return 'href' in props;
 }
 
 export default function Button(props: ButtonProps | AnchorProps) {
-  if (props.el === "anchor") {
-    return <></>
+  const baseClassNames = 'rounded-md px-3.5 py-2.5 text-sm font-semibold';
+
+  const colorClassNames = {
+    primary:
+      'text-white shadow-sm bg-primary hover:bg-primary-strong active:bg-primary-stronger focus:outline-none focus:ring focus:ring-neutral',
+    secondary:
+      'text-white shadow-sm bg-secondary hover:bg-secondary-strong active:bg-secondary-stronger focus:outline-none focus:ring focus:ring-neutral',
+    onlyText: 'leading-6 text-gray-strong',
+  };
+
+  if (isAnchorProps(props)) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <a
+          className={`${props.className} ${baseClassNames} ${
+            colorClassNames[props.color]
+          } ${props.additionalClassName}`}
+          {...props}
+        ></a>
+      </div>
+    );
   }
-  // return (<button {...props}></button>)
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <button
+        type={props.type}
+        className={`${props.className} ${baseClassNames} ${
+          colorClassNames[props.color]
+        } ${props.additionalClassName}`}
+        {...props}
+      ></button>
+    </div>
+  );
 }

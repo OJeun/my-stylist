2492fe0bd3a425731eb5104b9916aead0b75d879
Diane Choi <ekska21@gmail.com>
2024-07-02 05:35:49 -0700

    Create frontend output generator page

--- frontend/src/pages/OutfitGenerator.tsx
+++ frontend/src/pages/OutfitGenerator.tsx
@@ -1,7 +1,12 @@
 import ItemsGrid from '../components/ItemsGrid';
+import Button from '../components/ui/Button';
 import Input from '../components/ui/Input';
+import { useState } from 'react';
+import { clothingCategory } from '../components/ItemCard';
 
 export default function OutfitGenerator() {
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
   const inputClassName =
     'w-5 h-5 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-primary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600';
   const labelClassName = 'ms-2 text-lg';
@@ -12,12 +17,21 @@ export default function OutfitGenerator() {
       <div className="flex flex-1">
         <div className="pr-6">
           <ul className="text-xl tracking-tight text-gray-900">
-            <li>Top</li>
-            <li>Bottom</li>
-            <li>Jacket</li>
-            <li>Shoes</li>
-            <li>Bags</li>
-            <li>Accessories</li>
+            {clothingCategory.map((item) => (
+              <li
+                key={item}
+                className={`${
+                  selectedItem === item ? 'text-primary' : ''
+                } hover:cursor-pointer`}
+                onClick={() => {
+                  if (selectedItem !== item) {
+                    setSelectedItem(item);
+                  }
+                }}
+              >
+                {item}
+              </li>
+            ))}
           </ul>
         </div>
 
@@ -29,17 +43,14 @@ export default function OutfitGenerator() {
           <h3 className="mb-5 text-lg font-medium text-gray-strong dark:text-white">
             Select the type of clothes you want to match!
           </h3>
-
-          <ItemsGrid />
-
+          <ItemsGrid isInput={true} />
           <h3 className="my-5 text-lg font-medium text-gray-strong dark:text-white">
             Which type of clothing would you like to match?
           </h3>
 
           {/* Category checkboxes */}
-
           <div className="flex justify-center">
-            <div className="grid grid-cols-3 gap-x-20 gap-y-5">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-5">
               <Input
                 id="checkbox"
                 type="checkbox"
@@ -84,6 +95,9 @@ export default function OutfitGenerator() {
               />
             </div>
           </div>
+          <Button color="secondary" additionalclassname="w-80 m-5">
+            Get Styled!
+          </Button>
         </div>
       </div>
     </div>

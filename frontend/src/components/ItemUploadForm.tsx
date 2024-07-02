ae2473617e8054f359ceeef8f7c52204ce94d474
Diane Choi <ekska21@gmail.com>
2024-07-02 16:51:46 -0700

    Add close button in a modal

--- frontend/src/components/ItemUploadForm.tsx
+++ frontend/src/components/ItemUploadForm.tsx
@@ -6,6 +6,7 @@ import { InputProps } from './ui/Input';
 
 export default function ItemUploadForm() {
   const [open, setOpen] = useState(true);
+  const [imageBase64, setImageBase64] = useState<string | null>(null);
 
   const categories: InputProps[] = [
     { id: 'checkbox', type: 'checkbox', label: 'Top' },
@@ -21,6 +22,23 @@ export default function ItemUploadForm() {
     { id: 'summer-checkbox', type: 'checkbox', label: 'Summer' },
     { id: 'winter-checkbox', type: 'checkbox', label: 'Winter' },
   ];
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const base64 = reader.result;
+      setImageBase64(base64 as string);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <Dialog
@@ -41,18 +59,42 @@ export default function ItemUploadForm() {
           >
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div className="flex flex-col items-center space-y-4">
+                {/* <!--Close button--> */}
+                <button
+                  type="button"
+                  className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none absolute top-3 right-3"
+                  onClick={() => setOpen(false)}
+                  aria-label="Close"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
                 <div
                   className="border-dashed border-2 border-primary p-8 text-center w-[200px] h-[200px] flex items-center"
+                  onDrop={handleDrop}
+                  onDragOver={handleDragOver}
                   style={{ border: '2px dashed #ccc' }}
                 >
                   <p>Drag & Drop Your Clothing Photo!</p>
                 </div>
                 <div>
-                  <h1>Styles</h1>
+                  <h5 className="text-2xl font-medium">Styles</h5>
                   <InputGroup inputs={categories} />
                 </div>
                 <div>
-                  <h1>Seasons</h1>
+                  <h5 className="text-2xl font-medium">Seasons</h5>
                   <InputGroup inputs={seasons} />
                 </div>
               </div>

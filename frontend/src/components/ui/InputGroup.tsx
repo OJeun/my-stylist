60f0f7625240a3b0f4598fdacd11b43e2d779587
Diane Choi <ekska21@gmail.com>
2024-07-03 16:27:30 -0700

    Add ComponentPropsWithoutRef for flexible layout

--- frontend/src/components/ui/InputGroup.tsx
+++ frontend/src/components/ui/InputGroup.tsx
@@ -1,26 +1,29 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, ComponentPropsWithoutRef } from 'react';
 import Input, { InputProps } from './Input';
 
 type InputGroupProps = {
   inputs: InputProps[];
   selected: string | null;
   setSelected: (selected: string) => void;
-};
+} & ComponentPropsWithoutRef<'div'>;
 
-
-
-export default function InputGroup({ inputs, selected, setSelected }: InputGroupProps) {
+export default function InputGroup({
+  inputs,
+  selected,
+  setSelected,
+  ...props
+}: InputGroupProps) {
   const inputClassName =
     'w-5 h-5 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-primary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600';
   const labelClassName = 'ms-2 text-lg';
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { id } = e.target;
-    setSelected(id)
-  }
+    setSelected(id);
+  };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-5">
+    <div {...props}>
       {inputs.map((input) => (
         <Input
           key={input.id}

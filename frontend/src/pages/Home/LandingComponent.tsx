4a8949d69cc1258812485b66c3d42df3b3ce15ac
Diane Choi <ekska21@gmail.com>
2024-07-05 16:32:59 -0700

    Add image placeholder on landing page

--- frontend/src/pages/Home/LandingComponent.tsx
+++ frontend/src/pages/Home/LandingComponent.tsx
@@ -4,10 +4,13 @@ export default function LandingComponent() {
   return (
     <div className="relative isolate lg:px-8 flex space-x-5">
       <div className="w-1/2">
-        <img className="h-full w-full object-cover" src="" alt="Landing" />
+        <img
+          className="h-full w-full object-cover"
+          src="https://st2.depositphotos.com/3261171/5213/i/450/depositphotos_52139673-stock-photo-dj-in-tuxedo-mixing-by.jpg"
+          alt="Landing"
+        />
       </div>
       <div className="w-1/2 mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
-
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
             Hello World

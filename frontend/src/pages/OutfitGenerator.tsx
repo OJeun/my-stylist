4435520eacf9f7998619f7e6b0073a2801ed1e91
Diane Choi <ekska21@gmail.com>
2024-07-05 16:32:44 -0700

    Organize left and right container

--- frontend/src/pages/OutfitGenerator.tsx
+++ frontend/src/pages/OutfitGenerator.tsx
@@ -1,26 +1,26 @@
-import ItemsGrid from "../components/ItemsGrid";
-import Button from "../components/ui/Button";
-import { InputProps } from "../components/ui/Input";
-import { useState } from "react";
-import { ClothingItem, clothingCategory } from "../components/ItemCard";
-import InputGroup from "../components/ui/InputGroup";
+import ItemsGrid from '../components/ItemsGrid';
+import Button from '../components/ui/Button';
+import { InputProps } from '../components/ui/Input';
+import { useState } from 'react';
+import { ClothingItem, clothingCategory } from '../components/ItemCard';
+import InputGroup from '../components/ui/InputGroup';
 
 export const categories: InputProps[] = [
-  { id: "checkbox", type: "checkbox", label: "TOP" },
-  { id: "bottom-checkbox", type: "checkbox", label: "BOTTOM" },
-  { id: "outer-checkbox", type: "checkbox", label: "OUTER" },
-  { id: "shose-checkbox", type: "checkbox", label: "SHOES" },
-  { id: "bag-checkbox", type: "checkbox", label: "BAG" },
-  { id: "accessory-checkbox", type: "checkbox", label: "ACCESSORY" },
+  { id: 'checkbox', type: 'checkbox', label: 'TOP' },
+  { id: 'bottom-checkbox', type: 'checkbox', label: 'BOTTOM' },
+  { id: 'outer-checkbox', type: 'checkbox', label: 'OUTER' },
+  { id: 'shose-checkbox', type: 'checkbox', label: 'SHOES' },
+  { id: 'bag-checkbox', type: 'checkbox', label: 'BAG' },
+  { id: 'accessory-checkbox', type: 'checkbox', label: 'ACCESSORY' },
 ];
 
 export default function OutfitGenerator() {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const [selectedCategoryCheckbox, setCategoryCheckbox] = useState<
     string | string[]
-  >([""]);
+  >(['']);
   const [selectedItem, setSelectedItem] = useState<ClothingItem | null>(null);
-  const [generatedImage, setgeneratedImage] = useState<string>("");
+  const [generatedImage, setgeneratedImage] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleSelectItem = (item: ClothingItem) => {
@@ -37,26 +37,26 @@ export default function OutfitGenerator() {
     };
 
     try {
-      console.log("Sending data to backend:", data);
+      console.log('Sending data to backend:', data);
 
-      const response = await fetch("/api/ai-generator", {
-        method: "POST",
+      const response = await fetch('/api/ai-generator', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
       });
 
       if (response.ok) {
         const result = await response.json();
-        console.log("Successfully received response from AI generator");
+        console.log('Successfully received response from AI generator');
         console.log(result.image);
         setgeneratedImage(result.image);
       } else {
-        console.error("Failed to generate AI recommendation");
+        console.error('Failed to generate AI recommendation');
       }
     } catch (error) {
-      console.error("Error occurred while fetching data:", error);
+      console.error('Error occurred while fetching data:', error);
     } finally {
       setLoading(false);
     }
@@ -65,17 +65,17 @@ export default function OutfitGenerator() {
   return (
     <div className="">
       <h1 className="">Outfit Suggestion</h1>
-      <div className="flex justify-between items-center">
-        {/* item card */}
-        <div className="">
+      <div className="flex items-center">
+        <div>
           <div className="flex justify-center">
+            {/* left container */}
             <div className="pr-14">
-              <ul className="tracking-tight text-gray-900">
+              <ul className="tracking-tight text-gray-strong">
                 {clothingCategory.map((category) => (
                   <li
                     key={category}
                     className={`${
-                      selectedCategory === category ? "text-primary" : ""
+                      selectedCategory === category ? 'text-primary' : ''
                     } hover:cursor-pointer mr-4 mb-4 text-lg`}
                     onClick={() => {
                       if (selectedCategory !== category) {
@@ -88,6 +88,8 @@ export default function OutfitGenerator() {
                 ))}
               </ul>
             </div>
+
+            {/* right container */}
             <div>
               <h3 className="mb-5 text-lg font-medium text-gray-strong">
                 Select the type of clothes you want to match!
@@ -96,57 +98,58 @@ export default function OutfitGenerator() {
               <h3 className="my-5 text-lg font-medium text-gray-strong">
                 Which type of clothing would you like to match?
               </h3>
+
+              {/* Category checkboxes */}
+              <div className="flex justify-center">
+                <InputGroup
+                  inputs={categories}
+                  selected={selectedCategoryCheckbox}
+                  setSelected={setCategoryCheckbox}
+                  className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-5"
+                ></InputGroup>
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <Button
+                  color="secondary"
+                  additionalclassname="w-80 m-5"
+                  onClick={handleAIrequest}
+                >
+                  Get Styled!
+                </Button>
+                {loading && (
+                  <div role="status">
+                    <div role="status">
+                      <svg
+                        aria-hidden="true"
+                        className="w-11 h-11 text-gray-light animate-spin fill-primary"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentFill"
+                        />
+                      </svg>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  </div>
+                )}
+              </div>
+              {generatedImage && !loading && (
+                <img
+                  src={generatedImage}
+                  alt="fail"
+                  className="w-full h-full object-cover"
+                ></img>
+              )}
             </div>
           </div>
-          {/* Category checkboxes */}
-          <div className="flex justify-center">
-            <InputGroup
-              inputs={categories}
-              selected={selectedCategoryCheckbox}
-              setSelected={setCategoryCheckbox}
-              className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-5"
-            ></InputGroup>
-          </div>
-
-          <div className="flex flex-col items-center justify-center">
-            <Button
-              color="secondary"
-              additionalclassname="w-80 m-5"
-              onClick={handleAIrequest}
-            >
-              Get Styled!
-            </Button>
-            {loading && (
-              <div role="status">
-                <div role="status">
-                  <svg
-                    aria-hidden="true"
-                    className="w-11 h-11 text-gray-light animate-spin fill-primary"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentFill"
-                    />
-                  </svg>
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </div>
-            )}
-          </div>
-          {generatedImage && !loading && (
-            <img
-              src={generatedImage}
-              alt="fail"
-              className="w-full h-full object-cover"
-            ></img>
-          )}
         </div>
       </div>
     </div>

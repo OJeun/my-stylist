2721bbfddf4e430e2dd319a6d4c4e33c0ea5c105
Diane Choi <ekska21@gmail.com>
2024-07-05 16:13:28 -0700

    Make footer bottom sticky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,14 @@ import AlertMessage from './components/AlertMessage';
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="mx-8 sm:mx-12 md:mx-24 lg:mx-48 xl:mx-80">
+      <div className="flex-grow mx-8 sm:mx-12 md:mx-24 lg:mx-48 xl:mx-80">
         <Outlet />
         <AlertMessage />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,6 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    // <footer className="text-center">
-    //   <p>© {currentYear} Developed by Julie Oh and Diane Choi</p>
-    // </footer>
     <footer className="bg-neutral text-center dark:bg-neutral-strong lg:text-left">
       <div className="p-4 text-center text-neutral-strong dark:text-neutral">
         © {currentYear} Copyright: blah blah blah

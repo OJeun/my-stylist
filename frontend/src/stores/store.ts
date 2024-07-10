16dca2678157f18ec1e51d451b426d81febd53d7
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-10 16:49:49 -0700

    Merge auth store to reducer store

--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -1,6 +1,7 @@
 import { FavourtieItemSlice } from "./features/favouriteItems";
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import createStore from "react-auth-kit/createStore";
 
 export const store = configureStore({
   reducer: {
@@ -12,3 +13,17 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppDispatch: () => typeof store.dispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+
+export interface IUserData {
+  name: string;
+  uuid: string;
+  email: string;
+}
+
+export const authStore = createStore<IUserData>({
+  authName: "_auth",
+  authType: "cookie",
+  cookieDomain: window.location.hostname,
+  cookieSecure: false,
+});

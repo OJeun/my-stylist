1660b2d331c93311662ce83c035597ad42744edb
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-10 20:22:21 -0700

    Use selector and fetch dispatch to populate favourite Item page

--- frontend/src/pages/Favorites.tsx
+++ frontend/src/pages/Favorites.tsx
@@ -1,8 +1,27 @@
+import React, { useEffect } from 'react';
+import { fetchFavouriteItems } from "../stores/features/favouriteItems";
+import { useAppDispatch, useAppSelector } from "../stores/store";
+
 export default function Favorites() {
-  
+  const dispatch = useAppDispatch();
+  const fetchedFavouriteItems = useAppSelector(state=> 
+    state.favouriteItem.favouriteItems
+  );
+  console.log(fetchedFavouriteItems)
+
+  useEffect(() => {
+    dispatch(fetchFavouriteItems()); 
+  }, [dispatch]);
+
   return (
     <>
       <h1>My Favorites</h1>
+
+      {fetchedFavouriteItems.map((favouriteItem) => (
+        <img src={favouriteItem.selectedItem} alt='alt' />
+      )
+    )}
+
     </>
   );
 }

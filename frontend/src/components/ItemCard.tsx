11a94a797522f583660b3daba7213629b7d5bf19
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-11 20:33:15 -0700

    Fix hard coded className by changing it to input

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -1,15 +1,17 @@
-import { XMarkIcon } from '@heroicons/react/24/outline';
-import Button from './ui/Button';
-import Input from './ui/Input';
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import { ComponentPropsWithoutRef } from "react";
+import { ImageAndID } from "./ItemsGrid";
 
 // ItemCard consist of Input componenet or Button Component
 export const clothingCategory = [
-  'Top',
-  'Bottom',
-  'Jacket',
-  'Shoes',
-  'Bags',
-  'Accessories',
+  "Top",
+  "Bottom",
+  "Jacket",
+  "Shoes",
+  "Bags",
+  "Accessories",
 ];
 
 export type ClothingItem = {
@@ -20,35 +22,41 @@ export type ClothingItem = {
 };
 
 type ItemCardProps = {
-  clothing: ClothingItem;
+  clothing: ImageAndID;
   index: number;
   isInput: boolean;
   isSelected: boolean;
+  inputClassName?: string;
+  labelClassName?: string;
   onSelect: () => void;
-};
+} & ComponentPropsWithoutRef<"input">;
+
+const baseInputClass = "group-hover:opacity-75 inline-flex items-center border-gray-light border-2 w-full h-full bg-white rounded-lg cursor-pointer overflow-hidden rounded-md relative"
 
 export default function ItemCard({
   clothing,
   index,
   isInput,
   isSelected,
+  inputClassName,
+  labelClassName,
   onSelect,
 }: ItemCardProps) {
   return (
     <div key={clothing.id} className="group relative aspect-w-1 aspect-h-1">
-      <div className="mb-4 h-[200px] w-[200px] overflow-hidden rounded-md">
+      <div className="mb-4 sm:h-36 md:h-40 lg:h-44 xl:h-48 w-32 sm:w-36 md:w-40 lg:w-44 xl:w-48 overflow-hidden rounded-md">
         {isInput ? (
           <>
             <Input
               id={`clothing-${index}`}
               type="radio"
-              inputClassName="peer text-primary border-gray-light border-2 focus:ring-primary focus:ring-2"
+              inputClassName={inputClassName}
               name="clothing"
               value={clothing.id}
               required
-                          checked={isSelected}
-            onChange={onSelect}
-              labelClassName={`group-hover:opacity-75 inline-flex items-center border-gray-light border-2 w-full h-full bg-white rounded-lg cursor-pointer overflow-hidden rounded-md relative`}
+              checked={isSelected}
+              onChange={onSelect}
+              labelClassName={labelClassName}
               imageSrc={clothing.imageSrc}
               imageAlt={clothing.imageAlt}
             ></Input>
@@ -56,12 +64,14 @@ export default function ItemCard({
         ) : (
           <>
             <Button className="absolute top-2 right-2 z-10">
-            <XMarkIcon className="w-6 h-6"/>
+              <XMarkIcon className="w-6 h-6" />
             </Button>
-          <img
+            <img
               src={clothing.imageSrc}
               alt={clothing.imageAlt}
-              className="w-full h-full object-cover" /></>
+              className="w-full h-full object-cover"
+            />
+          </>
         )}
       </div>
     </div>
--- frontend/src/components/ItemsGrid.tsx
+++ frontend/src/components/ItemsGrid.tsx
@@ -1,53 +1,68 @@
-import { useState } from 'react';
-import ItemCard, { ClothingItem } from './ItemCard';
+import { ComponentPropsWithoutRef, useState } from "react";
+import ItemCard, { ClothingItem } from "./ItemCard";
 
 type ClothingListProps = {
   isInput: boolean;
-  onSelectItem?: (item: ClothingItem) => void; 
+  onSelectItem?: (item: ClothingItem) => void;
+  clothingImages?: string[];
+  inputClassName?: string;
+  labelClassName?: string;
+} & ComponentPropsWithoutRef<"input">;
+
+export type ImageAndID = {
+  id: string | number;
+  imageSrc: string;
+  imageAlt: string;
 };
 
-export default function ItemsGrid({ isInput, onSelectItem }: ClothingListProps) {
-    const [clothings, setClothings] = useState<ClothingItem[]>([
+export default function ItemsGrid({
+  isInput,
+  onSelectItem,
+  clothingImages,
+  inputClassName,
+  labelClassName,
+}: ClothingListProps) {
+  const [clothings, setClothings] = useState<ClothingItem[]>([
     {
       id: 1,
-      name: 'Basic Tee',
+      name: "Basic Tee",
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
       imageAlt: "Front of men's Basic Tee in black.",
     },
     {
       id: 2,
-      name: 'Pretty Tee',
+      name: "Pretty Tee",
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
       imageAlt: "Front of men's Basic Tee in black.",
     },
     {
       id: 3,
-      name: 'Pretty Tee',
+      name: "Pretty Tee",
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
       imageAlt: "Front of men's Basic Tee in black.",
     },
     {
       id: 4,
-      name: 'Pretty Tee',
+      name: "Pretty Tee",
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
       imageAlt: "Front of men's Basic Tee in black.",
     },
     {
       id: 5,
-      name: 'Pretty Tee',
+      name: "Pretty Tee",
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
       imageAlt: "Front of men's Basic Tee in black.",
     },
     {
       id: 6,
-      name: 'Pretty Tee',
+      name: "Pretty Tee",
       imageSrc:
-        'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
       imageAlt: "Front of men's Basic Tee in black.",
     },
   ]);
@@ -56,26 +71,36 @@ export default function ItemsGrid({ isInput, onSelectItem }: ClothingListProps) 
 
   const handleSelectClothing = (index: number) => {
     setSelectedClothing(index);
-    if (onSelectItem)
-      onSelectItem(clothings[index])
+    if (onSelectItem) onSelectItem(clothings[index]);
   };
-  
 
+  console.log(clothingImages)
+  const clothingList = clothingImages
+    ? clothingImages.map((image, index) => ({
+        id: index + 1,
+        imageSrc: image,
+        imageAlt: `index ${index}`
+      }))
+    : clothings;
 
-  return (
-    <>
-       <div className="mt-6 flex overflow-x-scroll gap-4 sm:gap-6 px-4 sm:px-6 md:px-8 mx-auto max-w-full sm:max-w-lg md:max-w-2xl lg:max-w-2xl xl:max-w-2xl">
-        {clothings.map((clothing, index) => (
-          <ItemCard
-            key={clothing.id}
-            clothing={clothing}
-            index={index}
-            isSelected={selectedClothing === index}
-            onSelect={() => handleSelectClothing(index)}
-            isInput={isInput}
-          />
-        ))}
-      </div>
-    </>
-  );
+    console.log(clothingList)
+
+    return (
+      <>
+        <div className="mt-6 flex overflow-x-scroll gap-4 sm:gap-6 px-4 sm:px-6 md:px-8 mx-auto max-w-full sm:max-w-lg md:max-w-2xl lg:max-w-2xl xl:max-w-2xl">
+          {clothingList.map((items, index) => (
+            <ItemCard
+              key={index}
+              inputClassName={inputClassName}
+              labelClassName={labelClassName}
+              clothing={items}
+              index={index}
+              isSelected={selectedClothing === index}
+              onSelect={() => handleSelectClothing(index)}
+              isInput={isInput}
+            />
+          ))}
+        </div>
+      </>
+    );
 }

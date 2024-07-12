362ad7d0b4d198beff674abf06080540d89ae1c0
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-12 15:51:31 -0700

    Change debugging and test lines

--- backend/src/api/saveFavourite.ts
+++ backend/src/api/saveFavourite.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/save-favourtie', (req, res) => {
   const { selectedItem, generatedImages} = req.body;
-  console.log('Received signup request:', selectedItem, generatedImages);
+  console.log('Received favourite item:', selectedItem, generatedImages);
 
   res.json({message: "Successfully saved!"})
 

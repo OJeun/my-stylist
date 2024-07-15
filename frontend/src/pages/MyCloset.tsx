cad38b4c3985f52e83e8220e388bc79290e399fc
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-15 15:53:46 -0700

    Fetch closet items and pass that state to itemGrid

--- frontend/src/pages/MyCloset.tsx
+++ frontend/src/pages/MyCloset.tsx
@@ -1,17 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ItemsGrid from '../components/ItemsGrid';
 import Button from '../components/ui/Button';
 import ItemUploadForm from '../components/ItemUploadForm';
 import Dropdown from '../components/ui/Dropdown';
+import { useAppDispatch, useAppSelector } from '../stores/store';
+import { addClosetItems, fetchClosetItems } from '../stores/features/closetItems';
 
 export default function MyCloset() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const categories = ['Top', 'Bottom', 'Outer', 'Shoes', 'Bag', 'Accessory'];
+  const dispatch = useAppDispatch();
+
+  const fetchedClosetItems = useAppSelector(
+    (state) => state.closetItem.closetItems
+  );
+  const fetchedImages = fetchedClosetItems.map((item, index) => item.imageString);
+
+  const fetchedDataFromUploadForm = useAppSelector(
+    (state) => state.categorySeasonImage
+  );
+
+  console.log(fetchedDataFromUploadForm)
+  useEffect(() => {
+    dispatch(fetchClosetItems("top"));
+  }, [dispatch]);
 
   const handleItemAdd = async () => {
     setIsModalOpen(true);
     try {
-      // TODO
     } catch (error) {
       console.log(error);
     }
@@ -30,7 +46,7 @@ export default function MyCloset() {
           Add Your Item
         </Button>
       </div>
-      <ItemsGrid isInput={false} />
+      <ItemsGrid isInput={false} clothingImages={fetchedImages} imageClassName="object-cover max-h-full max-w-full mx-auto"/>
       {isModalOpen && (
         <ItemUploadForm
           isModalOpen={isModalOpen}

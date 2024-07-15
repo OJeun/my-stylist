84df36a4277c83e533f0dd9514a47d0e053738e0
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-15 16:29:43 -0700

    Add css to closet Images

--- frontend/src/pages/MyCloset.tsx
+++ frontend/src/pages/MyCloset.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from 'react';
-import ItemsGrid from '../components/ItemsGrid';
-import Button from '../components/ui/Button';
-import ItemUploadForm from '../components/ItemUploadForm';
-import Dropdown from '../components/ui/Dropdown';
-import { useAppDispatch, useAppSelector } from '../stores/store';
-import { addClosetItems, fetchClosetItems } from '../stores/features/closetItems';
+import { useEffect, useState } from "react";
+import ItemsGrid from "../components/ItemsGrid";
+import Button from "../components/ui/Button";
+import ItemUploadForm from "../components/ItemUploadForm";
+import Dropdown from "../components/ui/Dropdown";
+import { useAppDispatch, useAppSelector } from "../stores/store";
+import {
+  addClosetItems,
+  fetchClosetItems,
+} from "../stores/features/closetItems";
 
 export default function MyCloset() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const categories = ['Top', 'Bottom', 'Outer', 'Shoes', 'Bag', 'Accessory'];
+  const categories = ["Top", "Bottom", "Outer", "Shoes", "Bag", "Accessory"];
   const dispatch = useAppDispatch();
 
   const fetchedClosetItems = useAppSelector(
     (state) => state.closetItem.closetItems
   );
-  const fetchedImages = fetchedClosetItems.map((item, index) => item.imageString);
-
-  const fetchedDataFromUploadForm = useAppSelector(
-    (state) => state.categorySeasonImage
+  const fetchedImages = fetchedClosetItems.map(
+    (item, index) => item.imageString
   );
 
-  console.log(fetchedDataFromUploadForm)
   useEffect(() => {
     dispatch(fetchClosetItems("top"));
   }, [dispatch]);
@@ -41,12 +41,17 @@ export default function MyCloset() {
         <Button
           color="secondary"
           onClick={handleItemAdd}
-          additionalclassname={'mx-2'}
+          additionalclassname={"mx-2"}
         >
           Add Your Item
         </Button>
       </div>
-      <ItemsGrid isInput={false} clothingImages={fetchedImages} imageClassName="object-cover max-h-full max-w-full mx-auto"/>
+      <ItemsGrid
+        isInput={false}
+        clothingImages={fetchedImages}
+        labelClassName="group-hover:opacity-75 inline-flex items-center border-gray-light border-2 w-full h-full bg-white rounded-lg cursor-pointer overflow-hidden rounded-md relative"
+        imageClassName="object-cover max-h-full max-w-full mx-auto"
+      />
       {isModalOpen && (
         <ItemUploadForm
           isModalOpen={isModalOpen}

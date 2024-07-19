53b8025d05304fbca79639dcaf7d7a14f606c32c
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-19 16:10:08 -0700

    Create confirm messagemodal ui component

---
+++ frontend/src/components/ui/ConformationModal.tsx
@@ -0,0 +1,48 @@
+interface ConfirmationModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  message: string;
+}
+export default function ConfirmationModal(
+    {isOpen, onClose, onConfirm, message} : ConfirmationModalProps) {
+
+    if (!isOpen) return null;
+    
+    return (
+        <>
+        <div
+          className="fixed inset-0 bg-gray-600 bg-opacity-50 z-50"
+          onClick={onClose}
+        ></div>
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div
+            className="bg-white p-6 rounded-lg shadow-lg relative"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+              onClick={onClose}
+            >
+              &times;
+            </button>
+            <h2 className="text-lg font-bold mb-4">{message}</h2>
+            <div className="flex justify-end gap-4">
+              <button
+                className="px-4 py-2 bg-primary text-white rounded hover:bg-blue-600"
+                onClick={onConfirm}
+              >
+                Confirm
+              </button>
+              <button
+                className="px-4 py-2 bg-gray text-white rounded hover:bg-gray-600"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+};

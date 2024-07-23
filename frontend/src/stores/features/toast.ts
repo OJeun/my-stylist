ffe4e4ec3fa3defbd7cf3ab580dc2a62976d6661
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-22 23:09:28 -0700

    Add visible props not to show duplicated errors at once

--- frontend/src/stores/features/toast.ts
+++ frontend/src/stores/features/toast.ts
@@ -3,24 +3,28 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface ToastState {
     message: string;
     type: 'info' | 'success' | 'warning' | 'error';
+    visible: boolean
 }
 
 const initialState: ToastState = {
     message: '',
-    type: 'info'
+    type: 'info',
+    visible: false
 }
 
 export const ToastSlice = createSlice({
     name: 'toast',
     initialState,
     reducers: {
-        setToast: (state: ToastState, action: PayloadAction<{ message: string; type: 'info' | 'success' | 'warning' | 'error' }>) => {
+        setToast: (state: ToastState, action: PayloadAction<{ message: string; type: 'info' | 'success' | 'warning' | 'error'; visible: boolean }>) => {
           state.message = action.payload.message;
           state.type = action.payload.type;
+          state.visible = action.payload.visible;
         },
         clearToast: (state: ToastState) => {
           state.message = '';
           state.type = 'info';
+          state.visible = false
         },
       },
 });

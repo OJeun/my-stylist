4ce7adb8e0ede552f6abfa0fe23ef074b4b9a0c1
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-23 16:48:50 -0700

    Make outfit page mobile responsive

--- frontend/src/pages/OutfitGenerator.tsx
+++ frontend/src/pages/OutfitGenerator.tsx
@@ -10,6 +10,7 @@ import { useAppDispatch, useAppSelector } from "../stores/store";
 import { ClosetItem, fetchClosetItems } from "../stores/features/closetItems";
 import { v4 as uuidv4 } from "uuid";
 import { clearToast, setToast } from "../stores/features/toast";
+import Dropdown from "../components/ui/Dropdown";
 
 export const categories: InputProps[] = [
   { id: "top-checkbox", type: "checkbox", label: "TOP" },
@@ -21,7 +22,9 @@ export const categories: InputProps[] = [
 ];
 
 export default function OutfitGenerator() {
-  const [selectedCategory, setSelectedCategory] = useState<string | null>("top");
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(
+    "top"
+  );
   const [selectedCategoryCheckbox, setCategoryCheckbox] = useState<
     string | string[]
   >([]);
@@ -34,9 +37,7 @@ export default function OutfitGenerator() {
   const [alertMessage, setAlertMessage] = useState<string>("");
   const dispatch = useAppDispatch();
 
-  const toastMessage = useAppSelector(
-    (state) => state.toast.message
-  )
+  const toastMessage = useAppSelector((state) => state.toast.message);
 
   const fetchedClosetItems = useAppSelector(
     (state) => state.closetItem.closetItems
@@ -48,35 +49,36 @@ export default function OutfitGenerator() {
 
   useEffect(() => {
     if (alertMessage) {
-      dispatch(setToast({ message: alertMessage, type: "warning", visible: true }));
+      dispatch(
+        setToast({ message: alertMessage, type: "warning", visible: true })
+      );
       const timer = setTimeout(() => {
         dispatch(clearToast());
         setAlertMessage("");
       }, 2000);
 
-      return () => clearTimeout(timer); 
+      return () => clearTimeout(timer);
     }
   }, [alertMessage, dispatch]);
-
 
   const handleSelectItem = (item: ClosetItem) => {
     setSelectedItem(item);
   };
 
   const handleAIrequest = async () => {
-    setLoading(false)
+    setLoading(false);
     if (fetchedClosetItems.length === 0) {
-      setAlertMessage(`Save your item category first!`)
+      setAlertMessage(`Save your item category first!`);
       return;
     }
 
     if (!selectedItem) {
-      setAlertMessage("Select an item to be matched!")
+      setAlertMessage("Select an item to be matched!");
       return;
     }
 
     if (selectedCategoryCheckbox.length === 0) {
-      setAlertMessage("Select at least one category!")
+      setAlertMessage("Select at least one category!");
       return;
     }
 
@@ -115,155 +117,168 @@ export default function OutfitGenerator() {
       dispatch(saveFavouriteItems(data));
     } catch (error) {
       console.error("Error occurred while saving data:", error);
-      dispatch(setToast({
-        message: error as string,
-        type: "error",
-        visible: true
-      }))
-      setTimeout(()=>{
-        dispatch(clearToast())
-      }, 2000)
+      dispatch(
+        setToast({
+          message: error as string,
+          type: "error",
+          visible: true,
+        })
+      );
+      setTimeout(() => {
+        dispatch(clearToast());
+      }, 2000);
     } finally {
-      dispatch(setToast({
-        message: "Succesfully added to Favourite!",
-        type: "success",
-        visible: false
-      }))
-      setTimeout(()=>{
-        dispatch(clearToast())
-      }, 2000)
+      dispatch(
+        setToast({
+          message: "Succesfully added to Favourite!",
+          type: "success",
+          visible: false,
+        })
+      );
+      setTimeout(() => {
+        dispatch(clearToast());
+      }, 2000);
       setLoading(false);
     }
   };
 
   return (
     <>
-      <h1 className="">Outfit Suggestion</h1>
-      <div className="flex items-center">
-        <div>
-          <div className="flex justify-center">
-            {/* left container */}
-            <div className="pr-14">
-              <ul className="tracking-tight text-gray-strong">
-                {clothingCategory.map((category) => (
-                  <li
-                    key={category}
-                    className={`${
-                      selectedCategory === category.toLowerCase()
-                        ? "text-primary"
-                        : ""
-                    } hover:cursor-pointer mr-4 mb-4 text-lg`}
-                    onClick={() => {
-                      if (selectedCategory !== category) {
-                        setSelectedCategory(category.toLowerCase());
-                        dispatch(fetchClosetItems(category.toLowerCase()));
-                      }
-                    }}
-                  >
-                    {category}
-                  </li>
-                ))}
-              </ul>
+      <div>
+        <section>
+          <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold mb-6">
+            Outfit Suggestion
+          </h1>
+          <h3 className="text-start mb-3 sm:text-xxs md:text-sm lg:text-lg font-medium text-gray-strong">
+            Select the type of clothes to be matched!
+          </h3>
+        </section>
+        <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start">
+       {/* Heading Section */}
+
+          {/* left container */}
+          {/* Dropdown for small screens */}
+          <div className="lg:hidden mb-3">
+            <Dropdown title="Category" categories={clothingCategory} />
+          </div>
+
+          {/* List for larger screens */}
+          <ul className="hidden lg:pr-10 md:pr-10 lg:block tracking-tight text-gray-strong">
+            {clothingCategory.map((category) => (
+              <li
+                key={category}
+                className={`${
+                  selectedCategory === category.toLowerCase()
+                    ? "text-primary"
+                    : ""
+                } hover:cursor-pointer mr-4 mb-4 text-lg`}
+                onClick={() => {
+                  if (selectedCategory !== category) {
+                    setSelectedCategory(category.toLowerCase());
+                    dispatch(fetchClosetItems(category.toLowerCase()));
+                  }
+                }}
+              >
+                {category}
+              </li>
+            ))}
+          </ul>
+
+          {/* right container */}
+            <div>
+            <ItemsGrid
+              isInput={true}
+              onSelectItem={handleSelectItem}
+              inputClassName={
+                "peer text-primary border-gray-light border-2 focus:ring-primary focus:ring-2"
+              }
+              labelClassName={
+                "group-hover:opacity-75 inline-flex items-center border-gray-light border-2 w-full h-full bg-white rounded-lg cursor-pointer overflow-hidden rounded-md relative"
+              }
+              clothingItems={fetchedClosetItems}
+            />
+            <h3 className="sm:text-xxs md:text-sm lg:text-lg font-medium text-gray-strong">
+              Which type of clothing would you like to match?
+            </h3>
+
+            {/* Category checkboxes */}
+            <div className="flex justify-center">
+              <InputGroup
+                inputs={categories}
+                selected={selectedCategoryCheckbox}
+                setSelected={setCategoryCheckbox}
+                className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-5"
+              ></InputGroup>
             </div>
 
-            {/* right container */}
-            <div>
-              <h3 className="mb-5 text-lg font-medium text-gray-strong">
-                Select the type of clothes you want to match!
-              </h3>
-              <ItemsGrid
-                isInput={true}
-                onSelectItem={handleSelectItem}
-                inputClassName={
-                  "peer text-primary border-gray-light border-2 focus:ring-primary focus:ring-2"
-                }
-                labelClassName={
-                  "group-hover:opacity-75 inline-flex items-center border-gray-light border-2 w-full h-full bg-white rounded-lg cursor-pointer overflow-hidden rounded-md relative"
-                }
-                clothingItems={fetchedClosetItems}
-              />
-              <h3 className="my-5 text-lg font-medium text-gray-strong">
-                Which type of clothing would you like to match?
-              </h3>
-
-              {/* Category checkboxes */}
-              <div className="flex justify-center">
-                <InputGroup
-                  inputs={categories}
-                  selected={selectedCategoryCheckbox}
-                  setSelected={setCategoryCheckbox}
-                  className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-5"
-                ></InputGroup>
+            <div className="flex flex-col items-center justify-center">
+              <Button
+                color="secondary"
+                additionalclassname="w-80 m-9"
+                onClick={handleAIrequest}
+              >
+                Get Styled!
+              </Button>
               </div>
-
-              <div className="flex flex-col items-center justify-center">
-                <Button
-                  color="secondary"
-                  additionalclassname="w-80 m-9"
-                  onClick={handleAIrequest}
-                >
-                  Get Styled!
-                </Button>
-                {loading && (
+              {loading && (
+                <div role="status">
                   <div role="status">
-                    <div role="status">
-                      <svg
-                        aria-hidden="true"
-                        className="w-11 h-11 text-gray-light animate-spin fill-primary"
-                        viewBox="0 0 100 101"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                          fill="currentColor"
-                        />
-                        <path
-                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                          fill="currentFill"
-                        />
-                      </svg>
-                      <span className="sr-only">Loading...</span>
-                    </div>
+                    <svg
+                      aria-hidden="true"
+                      className="w-11 h-11 text-gray-light animate-spin fill-primary"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
                   </div>
-                )}
-              </div>
-              {fetchedGeneratedItems.length > 0 && !loading && (
-                <>
-                  <h2 className="text-primary">Recommendation for You!</h2>
+                </div>
+              )}
+            </div>
+            {fetchedGeneratedItems.length > 0 && !loading && (
+              <>
+                <h2 className="text-primary">Recommendation for You!</h2>
 
-                  <div className="mt-6 sm:px-6 md:px-8 mx-auto max-w-full sm:max-w-lg md:max-w-1xl lg:max-w-2xl xl:max-w-3xl">
-                    <ItemsGrid
-                      clothingItems={fetchedGeneratedItems}
-                      wrapCustomClassName={`grid grid-cols-1 ${
-                        fetchedGeneratedItems.length > 3
-                          ? "sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3"
-                          : `sm:grid-cols-${Math.max(
-                              1,
-                              fetchedGeneratedItems.length - 2
-                            )} 
+                <div className="mt-6 sm:px-6 md:px-8 mx-auto max-w-full sm:max-w-lg md:max-w-1xl lg:max-w-2xl xl:max-w-3xl">
+                  <ItemsGrid
+                    clothingItems={fetchedGeneratedItems}
+                    wrapCustomClassName={`grid grid-cols-1 ${
+                      fetchedGeneratedItems.length > 3
+                        ? "sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3"
+                        : `sm:grid-cols-${Math.max(
+                            1,
+                            fetchedGeneratedItems.length - 2
+                          )} 
            md:grid-cols-${Math.max(1, fetchedGeneratedItems.length - 1)} 
            lg:grid-cols-${Math.max(1, fetchedGeneratedItems.length)} 
            xl:grid-cols-${Math.max(1, fetchedGeneratedItems.length)}`
-                      } gap-4 items-center justify-center `}
-                      inputClassName="hidden"
-                      labelClassName="group-hover:opacity-75 inline-flex items-center border-gray-light border-2 w-full h-full bg-white rounded-lg overflow-hidden relative"
-                      isInput={true}
-                    />
-                  </div>
-                  <div className="flex flex-col items-center justify-center">
-                    <Button
-                      color="secondary"
-                      additionalclassname="w-80 m-9"
-                      onClick={handleSaveFavourite}
-                    >
-                      Save to Favourite
-                    </Button>
-                  </div>
-                </>
-              )}
-            </div>
-          </div>
+                    } gap-4 items-center justify-center `}
+                    inputClassName="hidden"
+                    labelClassName="group-hover:opacity-75 inline-flex items-center border-gray-light border-2 w-full h-full bg-white rounded-lg overflow-hidden relative"
+                    isInput={true}
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <Button
+                    color="secondary"
+                    additionalclassname="w-80 m-9"
+                    onClick={handleSaveFavourite}
+                  >
+                    Save to Favourite
+                  </Button>
+                </div>
+              </>
+            )}
+
         </div>
       </div>
     </>

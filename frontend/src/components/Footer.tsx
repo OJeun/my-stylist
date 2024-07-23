dc049a176822fe0eef7a227e67b0c89a1e6c99ff
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-23 15:48:23 -0700

    Fix footer on the bottom of the page

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-neutral text-center dark:bg-neutral-strong lg:text-left">
+    <footer className="bg-neutral text-center dark:bg-neutral-strong lg:text-left fixed bottom-0 left-0 w-full">
       <div className="p-4 text-center text-neutral-strong dark:text-neutral">
         © {currentYear} Copyright: blah blah blah
       </div>

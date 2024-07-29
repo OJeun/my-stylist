e9b9868efb3d5ba9e131354bfc1d93d42be2dcde
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-29 16:48:29 -0700

    Change login function to use sqlite database

--- backend/src/api/login.ts
+++ backend/src/api/login.ts
@@ -1,22 +1,40 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import users from '../database/fakedata'
+import { findUserByEmail, verifyPassword } from '../database/user';
 
 const router = express.Router();
 const accessSecret = 'access-secret';
 
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  console.log('Received login request:', email, password);
 
-  const user = users.find(u => u.email === email && u.password === password);
-  if (user) {
-    const accessToken = jwt.sign({ name: user.name, uuid: user.uuid }, accessSecret, { expiresIn: '1h' });
-    res.json({ accessToken, name: user.name, uuid: user.uuid, email: user.email });
-  } else {
-    res.status(401).json({ message: 'Check the mssage' });
+  try {
+    const user = await findUserByEmail(email)
+
+    if (!user) {
+      res.status(401).json({ message: 'Cannot find the email' });
+    }
+
+    const isMatch = await verifyPassword(email, password)
+    if (isMatch) {
+      const accessToken = jwt.sign({ name: user.name, uuid: user.userId }, accessSecret, { expiresIn: '1h' });
+      res.json({ accessToken, name: user.name, uuid: user.userId, email: user.email });
+    } else {
+      res.status(401).json({ message: 'Password is not correct' });
+    }
+  } catch (error) {
+    res.status(500).send('Error logging in');
   }
+
+  // const user = users.find(u => u.email === email && u.password === password);
+  // if (user) {
+  //   const accessToken = jwt.sign({ name: user.name, uuid: user.uuid }, accessSecret, { expiresIn: '1h' });
+  //   res.json({ accessToken, name: user.name, uuid: user.uuid, email: user.email });
+  // } else {
+  //   res.status(401).json({ message: 'Check the mssage' });
+  // }
 });
 
 export default router;

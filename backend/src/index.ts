8ca8d6b8aa9d430b253e0a769cf62d5da9183daf
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-29 16:47:42 -0700

    Initialize database when backend starts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,19 +5,29 @@ import signupRouter from './api/signup';
 import generatorRouter from './api/aiGenerator'
 import saveFavouriteRouter from './api/saveFavourite'
 import cors from 'cors';
+import { initializeDatabase } from './database/db';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+initializeDatabase()
+.then(() => {
+  console.log('Database initialized successfully');
+  app.use('/api', loginRouter);
+  app.use('/api', signupRouter);
+  app.use('/api', generatorRouter);
+  app.use('/api', saveFavouriteRouter);
 
-app.use('/api', loginRouter);
-app.use('/api', signupRouter);
-app.use('/api', generatorRouter);
-app.use('/api', saveFavouriteRouter);
+  app.listen(8888, () => {
+    console.log(`Server running on port ${8888}`);
+  });
+})
+
+.catch((error) => {
+  console.error('Failed to initialize database:', error);
+  process.exit(1);
+})
 
 
-const PORT = 8888;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+

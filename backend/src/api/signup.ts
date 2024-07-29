28789a08894c2c0aba51642479cf6fd92e08c580
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-29 16:47:06 -0700

    Create signup functions to register users and send proper error meesage

--- backend/src/api/signup.ts
+++ backend/src/api/signup.ts
@@ -1,17 +1,25 @@
 import express from 'express';
-import { addUser } from '../database/fakedata'
+import { createUser, findUserByEmail } from '../database/user';
+
 
 const router = express.Router();
 
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
   const { name, email, password } = req.body;
-  console.log('Received signup request:', email, password, name);
 
-  addUser(email, password, name)
-  
-  res.json({message: "Successfully signed up!"})
+  try {
+    const existingUser = await findUserByEmail(email);
+    if (existingUser) {
+      return res.status(409).json({ message: 'Email already exists' });
+    }
 
+    await createUser(name, email, password);
+    res.status(201).json({ message: 'User registered successfully' });
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user'});
+  }
 });
 
 export default router;

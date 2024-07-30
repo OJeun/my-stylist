978bf02591dc59b373e1a8fc7ff9acfb5a9a7699
Diane Choi <ekska21@gmail.com>
2024-07-30 14:23:31 -0700

    Change user image placeholder to user icon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,6 +7,8 @@ import {
 } from '@headlessui/react';
 import { HeartIcon } from '@heroicons/react/24/outline';
 import { HeartIcon as HeartIconFilled } from '@heroicons/react/24/solid';
+import { UserIcon } from '@heroicons/react/24/outline';
+import { UserIcon as UserIconFilled } from '@heroicons/react/24/solid';
 import { useState } from 'react';
 
 function classNames(...classes: string[]) {
@@ -15,45 +17,52 @@ function classNames(...classes: string[]) {
 
 export default function Navbar() {
   const [isHeartClicked, setIsHeartClicked] = useState(false);
-  
+
   return (
     <Disclosure as="nav" className="bg-white">
-      {({ open }) => (
-        <>
-          <div className="mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between sm:justify-between">
-              <div className="flex justify-center flex-grow">
-                <a
-                  href="/"
-                  className="h-8 w-auto hover:text-primary-strong font-bold font-mono text-2xl justify-center text-primary"
-                >
-                  AI Stylist
-                </a>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Favorite button */}
-                <a
-                  href="/favorites"
-                  className="relative rounded-full bg-white p-1 text-gray-600 hover:text-gray-strong focus:text-primary focus:text-bold focus:ring-primary"
-                  onFocus={() => setIsHeartClicked(true)}
-                  onBlur={() => setIsHeartClicked(false)}
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View favorite outfits</span>
-                  { isHeartClicked ? <HeartIconFilled className="h-6 w-6" aria-hidden="true" /> : <HeartIcon className="h-6 w-6" aria-hidden="true" /> }
-                </a>
+      <div className="mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between sm:justify-between">
+          <div className="flex justify-center flex-grow">
+            <a
+              href="/"
+              className="h-8 w-auto hover:text-primary-strong font-bold font-mono text-2xl justify-center text-primary"
+            >
+              AI Stylist
+            </a>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* Favorite button */}
+            <a
+              href="/favorites"
+              className="relative rounded-full bg-white p-1 text-gray-600 hover:text-gray-strong focus:text-primary focus:text-bold focus:ring-primary"
+              onFocus={() => setIsHeartClicked(true)}
+              onBlur={() => setIsHeartClicked(false)}
+            >
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">View favorite outfits</span>
+              {isHeartClicked ? (
+                <HeartIconFilled className="h-6 w-6" aria-hidden="true" />
+              ) : (
+                <HeartIcon className="h-6 w-6" aria-hidden="true" />
+              )}
+            </a>
 
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
+            {/* Profile dropdown */}
+            <Menu as="div" className="relative ml-3">
+              {({ open }) => (
+                <>
                   <div>
                     <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      {open ? (
+                        <UserIconFilled
+                          className="h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      ) : (
+                        <UserIcon className="h-6 w-6" aria-hidden="true" />
+                      )}
                     </MenuButton>
                   </div>
                   <MenuItems
@@ -113,12 +122,12 @@ export default function Navbar() {
                       )}
                     </MenuItem>
                   </MenuItems>
-                </Menu>
-              </div>
-            </div>
+                </>
+              )}
+            </Menu>
           </div>
-        </>
-      )}
+        </div>
+      </div>
     </Disclosure>
   );
 }

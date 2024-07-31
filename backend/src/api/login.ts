e759e330ffdbee7c31389e46e2308cc5c5be9d16
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-30 18:30:47 -0700

    Change import line user to users file

--- backend/src/api/login.ts
+++ backend/src/api/login.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import users from '../database/fakedata'
-import { findUserByEmail, verifyPassword } from '../database/user';
+import { findUserByEmail, verifyPassword } from '../database/users';
 
 const router = express.Router();
 const accessSecret = 'access-secret';
@@ -19,8 +19,8 @@ router.post('/login', async (req, res) => {
 
     const isMatch = await verifyPassword(email, password)
     if (isMatch && user) {
-      const accessToken = jwt.sign({ name: user.name, uuid: user.id }, accessSecret, { expiresIn: '1h' });
-      res.json({ accessToken, name: user.name, uuid: user.id, email: user.email });
+      const accessToken = jwt.sign({ name: user.name, uuid: user.userId }, accessSecret, { expiresIn: '1h' });
+      res.json({ accessToken, name: user.name, uuid: user.userId, email: user.email });
     } else {
       res.status(401).json({ message: 'Password is not correct' });
     }

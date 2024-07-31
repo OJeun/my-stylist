ba4eecc7a1f15209497703166ab403a72dc4fbb4
Diane Choi <ekska21@gmail.com>
2024-07-30 18:31:16 -0700

    Add database schema

--- backend/src/database/schema.sql
+++ backend/src/database/schema.sql
@@ -1,7 +1,68 @@
-CREATE TABLE IF NOT EXISTS users (
-    id VARCHAR(200) NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS Users (
+    userId VARCHAR(200) NOT NULL UNIQUE,
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Clothes (
+    clothId INTEGER NOT NULL AUTOINCREMENT,
+    description TEXT NOT NULL,
+    imgSrc TEXT NOT NULL,
+    season INTEGER NOT NULL,
+    typeId INTEGER NOT NULL,
+    PRIMARY KEY (clothId),
+    FOREIGN KEY (season) REFERENCES Season(seasonId),
+    FOREIGN KEY (typeId) REFERENCES ClothingType(typeId)
+);
+
+CREATE TABLE IF NOT EXISTS ClothingType (
+    typeId INTEGER NOT NULL,
+    typeName VARCHAR(200) NOT NULL,
+    PRIMARY KEY (typeId)
+);
+
+CREATE TABLE IF NOT EXISTS Season (
+    seasonId INTEGER NOT NULL,
+    seasonName VARCHAR(200) NOT NULL,
+    PRIMARY KEY (seasonId)
+);
+
+CREATE TABLE IF NOT EXISTS UsersClothes (
+    userId VARCHAR(200) NOT NULL,
+    clothId INTEGER NOT NULL,
+    PRIMARY KEY (userId, clothId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE,
+    FOREIGN KEY (clothId) REFERENCES Clothes(clothId) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS UserFavoriteCombination (
+    favoriteCombinationId INTEGER NOT NULL AUTOINCREMENT,
+    userId VARCHAR(200) NOT NULL,
+    PRIMARY KEY (favoriteCombinationId, userId),
+    FOREIGN KEY (userId) REFERENCES Users(id)
+);
+
+CREATE TABLE IF NOT EXISTS FavoriteCombinationClothes (
+    favoriteCombinationId INTEGER NOT NULL,
+    clothId INTEGER NOT NULL,
+    PRIMARY KEY (favoriteCombinationId, clothId),
+    FOREIGN KEY (favoriteCombinationId) REFERENCES UserFavoriteCombination(favoriteCombinationId),
+    FOREIGN KEY (clothId) REFERENCES Clothes(clothId),
+);
+
+CREATE TABLE IF NOT EXISTS RecentlyViewedCombination (
+    recentCombinationId INTEGER NOT NULL AUTOINCREMENT,
+    clothId VARCHAR(200) NOT NULL,
+    PRIMARY KEY (recentCombinationId, clothId),
+    FOREIGN KEY (clothId) REFERENCES Clothes(clothId)
+);
+
+CREATE TABLE IF NOT EXISTS UserRecentlyViewedCombination (
+    recentCombinationId INTEGER NOT NULL,
+    userId VARCHAR(200) NOT NULL,
+    viewedAt TIMESTAMP NOT NULL,
+    PRIMARY KEY (recentCombinationId, userId),
+    FOREIGN KEY (recentCombinationId) REFERENCES RecentlyViewedCombination(recentCombinationId),
+    FOREIGN KEY (userId) REFERENCES Users(id)
+);

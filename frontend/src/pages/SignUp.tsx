16b8ad01f5827fa9330e4c6a7069cf5d44db8682
Diane Choi <ekska21@gmail.com>
2024-07-31 18:41:17 -0700

    Change the image in signup page

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -2,6 +2,7 @@ import axios, { AxiosError } from "axios";
 import { useFormik } from "formik";
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import clothing_icon from '../assets/icon_clothing.png';
 
 
 export default function SignUp() {
@@ -44,12 +45,11 @@ export default function SignUp() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
+            className="mx-auto h-20 w-auto"
+            src={clothing_icon}
+            alt="My Stylist"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign up to your account
@@ -154,8 +154,6 @@ export default function SignUp() {
               Log in
             </a>
           </p>
-
-
         </div>
       </div>
     </>

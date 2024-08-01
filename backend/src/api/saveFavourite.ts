0a72b58aa44156be7f4752dbf49e92e58c046e31
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-07-31 18:16:55 -0700

    Use addFavoriteCombination function in saveFavourite file

--- backend/src/api/saveFavourite.ts
+++ backend/src/api/saveFavourite.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import { addFavoriteCombination } from '../database/favorite';
 
 const router = express.Router();
 
 
 router.post('/save-favourtie', (req, res) => {
-  const { selectedItem, generatedImages} = req.body;
-  console.log('Received favourite item:', selectedItem, generatedImages);
-
+  const { selectedItem, generatedImages, userId} = req.body;
+  console.log('Received favourite item:', selectedItem, generatedImages, userId);
+  
+  // Save the favourite item to the database
+  addFavoriteCombination(userId, selectedItem, generatedImages);
   res.json({message: "Successfully saved!"})
 
 });

48056a510970fd713267bce661375ad56c56aae4
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-08-06 20:18:47 -0700

    Add mapping default images utli function

---
+++ frontend/src/utils/api/image.ts
@@ -0,0 +1,24 @@
+import { ClosetItem } from "../../stores/features/closetItems";
+
+export const setDefaultImg = (cloth: ClosetItem) => {
+    console.log("cloth", cloth.typeId.toString());
+    if (!cloth.imgSrc) {
+      switch (cloth.typeId.toString()) {
+        case "1":
+          return "/assets/top.png";
+        case "2":
+          return "/assets/bottom.png";
+        case "3":
+          return"./assets/outer.png";
+        case "4":
+          return"./assets/shoes.png";
+        case "5":
+          return "./assets/bag.png";
+        case "6":
+          return "./assets/accessories.png";
+        default:
+          return "./assets/default.png";
+      }
+    } else {
+      return cloth.imgSrc;
+  }};

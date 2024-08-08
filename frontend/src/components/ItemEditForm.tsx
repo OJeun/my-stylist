63cd8dbbeb721e6c8744edd90ead46f36c704a01
Diane Choi <ekska21@gmail.com>
2024-08-07 19:51:26 -0700

    Create ItemEditForm

---
+++ frontend/src/components/ItemEditForm.tsx
@@ -0,0 +1,221 @@
+import { SetStateAction, useState, useEffect } from 'react';
+import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react';
+import InputGroup from './ui/InputGroup';
+import Button from './ui/Button';
+import { InputProps } from './ui/Input';
+import { BsArrowRepeat } from 'react-icons/bs';
+import { categories } from '../pages/OutfitGenerator';
+import { ClosetItem, saveClosetItems } from '../stores/features/closetItems';
+import { useAppDispatch } from '../stores/store';
+import { setCategory } from '../stores/features/category';
+import Input from './ui/Input';
+import { convertTypeIdToCategory } from '../utils/api/getId';
+
+const seasons: InputProps[] = [
+  { id: 'spring-fall', type: 'checkbox', label: 'Spring/Fall' },
+  { id: 'summer', type: 'checkbox', label: 'Summer' },
+  { id: 'winter', type: 'checkbox', label: 'Winter' },
+];
+
+type ItemEditFormProps = {
+  isModalOpen: boolean;
+  setIsModalOpen: (isOpen: boolean) => void;
+  selectedClothing: ClosetItem;
+};
+
+export default function ItemEditForm({
+  isModalOpen,
+  setIsModalOpen,
+  selectedClothing,
+}: ItemEditFormProps) {
+  const handleSetSelectedSeason = (
+    value: SetStateAction<string | string[]>
+  ) => {
+    if (typeof value === 'function') {
+      setSelectedSeason((prev) => value(prev) as string[]);
+    } else if (typeof value === 'string') {
+      setSelectedSeason([value]);
+    } else {
+      setSelectedSeason(value);
+    }
+  };
+
+  const [imageBase64, setImageBase64] = useState<string | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [selectedSeason, setSelectedSeason] = useState<string[]>([]);
+  const [description, setDescription] = useState<string>('');
+
+  const dispatch = useAppDispatch();
+
+  const convertedTypeId = convertTypeIdToCategory(
+    Number(selectedClothing.typeId)
+  );
+
+  useEffect(() => {
+    if (selectedClothing) {
+      setImageBase64(selectedClothing.imgSrc);
+      setSelectedCategory(convertedTypeId?.toUpperCase() || '');
+      // setSelectedSeason(selectedClothing.season);
+      setDescription(selectedClothing.description);
+    }
+  }, [selectedClothing]);
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const base64 = reader.result;
+      setImageBase64(base64 as string);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleDeleteImage = () => {
+    setImageBase64(null);
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleFormSubmit = async () => {
+    if (!selectedCategory || !selectedSeason) {
+      alert('Please select both a category and a season.');
+      return;
+    }
+
+    const data: ClosetItem = {
+      userId: localStorage.getItem('uid') as string,
+      imgSrc: imageBase64 as string,
+      season: selectedSeason as string[],
+      typeId: selectedCategory as string,
+      description: description as string,
+    };
+    dispatch(saveClosetItems(data));
+    dispatch(setCategory(selectedCategory));
+
+    setIsModalOpen(false);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Dialog
+      className="relative z-10"
+      open={isModalOpen}
+      onClose={handleCloseModal}
+    >
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+          >
+            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <div className="flex flex-col items-center space-y-4">
+                {/* <!--Close button--> */}
+                <button
+                  type="button"
+                  className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none absolute top-3 right-3"
+                  onClick={handleCloseModal}
+                  aria-label="Close"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+
+                <div
+                  className="relative bg-white border-dashed rounded-lg p-8 border-2 border-gray-light text-center w-[150px] h-[150px] md:w-[200px] md:h-[200px] flex items-center justify-center"
+                  onDrop={handleDrop}
+                  onDragOver={handleDragOver}
+                >
+                  <div
+                    className="btn delete-button text-gray hover:text-black absolute top-2 right-2 hover:cursor-pointer"
+                    onClick={handleDeleteImage}
+                  >
+                    <BsArrowRepeat />
+                  </div>
+                  {!imageBase64 ? (
+                    <p className="text-gray">
+                      <strong>Drag & Drop</strong> Your Clothing Photo!
+                    </p>
+                  ) : (
+                    <img
+                      className="object-cover max-h-full max-w-full"
+                      src={imageBase64}
+                      alt="Uploaded"
+                    />
+                  )}
+                </div>
+                <div>
+                  <div>
+                    <h2>Category</h2>
+                    <p>{selectedCategory}</p>
+                  </div>
+                  <div>
+                    <h2 className="mt-[15px]">Season</h2>
+                    <InputGroup
+                      className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-5"
+                      inputs={seasons}
+                      selected={selectedSeason}
+                      setSelected={handleSetSelectedSeason}
+                      singleSelection={false}
+                    />
+                  </div>
+                  <div>
+                    <h2 className="mt-[15px]">Description</h2>
+                    <p className="mb-2 text-sm text-red">
+                      * This will be used for generating AI suggestion. Please
+                      describe as detailed as possible.
+                    </p>
+                    <Input
+                      type="text"
+                      id="description"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      inputClassName="w-full"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-6 flex justify-center space-x-4">
+                <Button color="textOnly" onClick={handleCloseModal}>
+                  Cancel
+                </Button>
+                <Button
+                  color="secondary"
+                  additionalclassname="w-30 sm: w-60"
+                  onClick={handleFormSubmit}
+                >
+                  Add Item
+                </Button>
+              </div>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
